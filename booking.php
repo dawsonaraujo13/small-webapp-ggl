29aa3975dfe563a3a2614f61e3e69b491e6ff55c
dawsonaraujo13 <dawsonaraujo13@gmail.com>
2021-03-02 18:06:46 -0500

    saved php

--- booking.php
+++ booking.php
@@ -1,32 +1,37 @@
 <?php
-    $first_name = $_POST['first'];
-    $last_name = $_POST['last'];
-    $visitor_email = $_POST['email'];
-    $phone_num = $_POST['phone'];
-    $instagram = $_POST['instagram'];
-    $service = $_POST['service'];
-    $date = $_POST['date'];
-    $time = $_POST['time'];
-    $pref_contact = $_POST['contact'];
-    $comment = $_POST_['comment'];
 
-    $email_from = $visitor_email;
-    $email_subject = 'New Booking';
-    $email_body = "Name: $first_name $last_name \n".
-        "Email: $visitor_email \n".
-        "Phone: $phone_num \n".
-        "Instagram: $instagram \n". 
-        "Preferred Contact: $pref_contact".
-        "Service Type: $service \n". 
-        "Date Requested: $date \n". 
-        "Time: $time \n". 
-        "Message: $comment";     
-        
-    $to = "genevieveslashes@gmail.com";
+    if(isset($_POST['submit'])){
+        $first_name = $_POST['first'];
+        $last_name = $_POST['last'];
+        $visitor_email = $_POST['email'];
+        $phone_num = $_POST['phone'];
+        $instagram = $_POST['instagram'];
+        $service = $_POST['service'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+        $pref_contact = $_POST['contact'];
+        $comment = $_POST_['comment'];
+    
+        $email_from = $visitor_email;
+        $email_subject = 'New Booking';
+        $email_body = "Name: $first_name $last_name \n".
+            "Email: $visitor_email \n".
+            "Phone: $phone_num \n".
+            "Instagram: $instagram \n". 
+            "Preferred Contact: $pref_contact".
+            "Service Type: $service \n". 
+            "Date Requested: $date \n". 
+            "Time: $time \n". 
+            "Message: $comment";     
+            
+        $to = "genevieveslashes@gmail.com";
+    
+        $headers = "From: $email_from \r\nReply-To: $visitor_email \r\n"
+    
+        mail($to,$email_subject,$email_body,$headers);
+    
+        header("Location: index.html");
+    }
 
-    $headers = "From: $email_from \r\nReply-To: $visitor_email \r\n"
 
-    mail($to,$email_subject,$email_body,$headers);
-
-    header("Location: index.html");
 ?>
